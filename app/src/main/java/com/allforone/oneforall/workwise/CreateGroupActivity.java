e6314e39095a1bd8b385281ba9042f7217ab81a0
my3hegde <maithri24@gmail.com>
2017-05-05 22:19:55 -0400

    Create Group Activity class and layout file

---
+++ app/src/main/java/com/allforone/oneforall/workwise/CreateGroupActivity.java
@@ -0,0 +1,19 @@
+package com.allforone.oneforall.workwise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Maithri on 2017-05-05.
+ */
+
+public class CreateGroupActivity extends Activity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.create_group_screen);
+
+}
+}
